52e9807e900d85a9420dd67199944301072e16d3
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2021-06-11 11:50:44 -0300

    Soma vizinhos

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -5,7 +5,38 @@ using std::endl;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+    int num[2];
+
+    cout << "Digite um numero inteiro de para referencia: " << endl;
+    cin >> num[1];
+
+    cout << "Digite um numero inteiro, para somar os anterios ou posteriores do numero de referencaia: " << endl;
+    cin >> num[2];
+
+    bool temp = num[2];
+    temp == true;
+
+    if (num[2] > 0)
+    {
+        while (!temp)
+        {
+            num[1]++;
+        }
+        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+    }
+    else if (num[2] = 0)
+    {
+        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+    }
+    
+    else
+    {
+        while (!temp)
+        {
+            num[1] = num[1] + num[1] -1;
+        }
+        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+    }
 
     return 0;
 }
