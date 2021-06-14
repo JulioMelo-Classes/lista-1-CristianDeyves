e558dfddef04a71262d9bbcc35f0118d7edf3e59
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2021-06-14 11:15:11 -0300

    Ajuste de questão

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -7,34 +7,33 @@ int main( void )
 {
     int num[2] = {0}, temp = 0;
 
-    cout << "Digite um numero inteiro de para referencia: " << endl;
+    cin >> num[0];
+
     cin >> num[1];
 
-    cout << "Digite um numero inteiro, para somar os anterios ou posteriores do numero de referencaia: " << endl;
-    cin >> num[2];
-
-    if (num[2] > 0)
+    if (num[1] > 0)
     {
-        while (temp != num[2])
+        while (temp != num[1])
         {
-            num[1]++, temp++;
+            num[0]++;
+            temp++;
         }
-        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+        cout << num[0] << endl;
     }
 
-    else if (num[2] = 0)
+    else if (num[1] = 0)
     {
-        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+        cout << num[0] << endl;
     }
 
     else
     {
-        while (temp != num[2])
+        while (temp != num[1])
         {
-            num[1] = num[1] + (num[1] -1);
+            num[0] = num[0] + (num[0] - 1);
             temp++;
         }
-        cout << "A soma dos antecedente/posteriores e: " << num[1] << endl;
+        cout << num[0] << endl;
     }
 
     return 0;
